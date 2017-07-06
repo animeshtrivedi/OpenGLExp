43d51b272fc9549f34db3db166a85a0fd119567d
Animesh Trivedi <animesh.trivedi@gmail.com>
2017-07-06 20:29:59 +0200

    short example where the second window does not refresh

--- dualwindow2.c
+++ dualwindow2.c
@@ -3,23 +3,25 @@
 //
 
 #include <GL/glut.h>
+#include <stdio.h>
 
-float clr = 0.2;
+float clr1 = 0.2;
+float clr2 = 0.6;
 
-static void displayX()
+static void displayX1()
 {
     // clear the draw buffer .
     glClear(GL_COLOR_BUFFER_BIT);   // Erase everything
 
     // set the color to use in draw
-    clr += 0.1;
-    if ( clr>1.0)
+    clr1 += 0.1;
+    if ( clr1 > 1.0)
     {
-        clr=0;
+        clr1 = 0;
     }
     // create a polygon ( define the vertexs)
     glBegin(GL_POLYGON); {
-        glColor3f(clr, clr, clr);
+        glColor3f(clr1, clr1, clr1);
         glVertex2f(-0.5, -0.5);
         glVertex2f(-0.5,  0.5);
         glVertex2f( 0.5,  0.5);
@@ -29,14 +31,57 @@ static void displayX()
     glFlush();
 }
 
+static void displayX2()
+{
+    // clear the draw buffer .
+    glClear(GL_COLOR_BUFFER_BIT);   // Erase everything
+
+    // set the color to use in draw
+    clr2 += 0.1;
+    if ( clr2 > 1.0)
+    {
+        clr2 = 0;
+    }
+    // create a polygon ( define the vertexs)
+    glBegin(GL_POLYGON); {
+        glColor3f(clr2, clr2, clr2);
+        glVertex2f(-0.5, -0.5);
+        glVertex2f(-0.5,  0.5);
+        glVertex2f( 0.5,  0.5);
+        glVertex2f( 0.5, -0.5);
+    } glEnd();
+
+    glFlush();
+}
+
+static void timer_redraw1(int value){
+    printf(" Execution1111 for 1000s \n");
+    // do something
+    glutPostRedisplay();
+    // 1000 milliseconds
+    glutTimerFunc(1000, timer_redraw1, 0);
+}
+
+static void timer_redraw2(int value){
+    printf(" Execution5555 for 500ms \n");
+    // do something
+    glutPostRedisplay();
+    // 1000 milliseconds
+    glutTimerFunc(500, timer_redraw2, 0);
+}
+
 // Main execution  function
 int dualwindow2_main(int argc, char *argv[])
 {
     glutInit(&argc, argv);      // Initialize GLUT
+
     glutCreateWindow("win1");   // Create a window 1
-    glutDisplayFunc(displayX);   // Register display callback
+    glutDisplayFunc(displayX1);   // Register display callback
+    glutTimerFunc(1000, timer_redraw1, 0);
+
     glutCreateWindow("win2");   // Create a window 2
-    glutDisplayFunc(displayX);   // Register display callback
+    glutDisplayFunc(displayX2);   // Register display callback
+    glutTimerFunc(500, timer_redraw2, 0);
 
     glutMainLoop();             // Enter main event loop
     return 0;
