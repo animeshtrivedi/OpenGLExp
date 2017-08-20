786b54a60067e6edefbaee88c1e1fe6a5aec8e0a
Animesh Trivedi <animesh.trivedi@gmail.com>
2017-08-20 15:35:20 +0200

    notes and script

---
+++ prepapre.sh
@@ -0,0 +1,4 @@
+sudo apt-get install git cmake build-essential autoconf libusb-1.0-0-dev freenect  libfreenect-demos libusb-1.0-0  libusb-1.0-0-dbg  libusb-1.0-0-dev  libusb-1.0-doc mesa-common-dev libglu1-mesa-dev  libglu1-mesa  freeglut3  freeglut3-dbg  freeglut3-dev  libxmu-dev libxi-dev mesa-utils xorg-dev libglu1-mesa-dev libsdl2-dev libglew-dev
+
+
+
